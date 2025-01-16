8c8429619730424995682e8592e43a068fb64215
Dodidee <nordiananorizan10@gmail.com>
2025-01-16 17:11:45 +0800

    upadate admin profile (picture, controller and admin data)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; // Import the Auth facade
 
+
 class AdminController extends Controller
 {
     public function destroy(Request $request)
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -5,49 +5,24 @@
 
         <div class="row">
             <div class="col-lg-4">
-                <div class="card">
-                    <img class="card-img-top img-fluid" src="assets/images/small/img-5.jpg" alt="Card image cap">
+            <div class="card"><br>
+            <center>
+                    <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/dee.jpg') }}" alt="Card image cap">
+            </center>
                     <div class="card-body">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a
-                            natural lead-in to additional content. This content is a little bit
-                            longer.</p>
-                        <p class="card-text">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </p>
+                        <h4 class="card-title">Name : {{ $adminData->name }}</h4>
+                        <hr>
+                        <h4 class="card-title">User Email : {{ $adminData->email }}</h4>
+                        <hr>
+                        <h4 class="card-title">Username : {{ $adminData->username }}</h4>
+                        <hr>
+                        <a href="" class="btn btn-info btn-rounded waves-effect waves-light">Edit Profile</a>
+                        
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a
-                            natural lead-in to additional content. This content is a little bit
-                            longer.</p>
-                        <p class="card-text">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </p>
-                    </div>
-                    <img class="card-img-bottom img-fluid" src="assets/images/small/img-7.jpg" alt="Card image cap">
-                </div>
-            </div>
 
-            <div class="col-lg-4">
-                <div class="card">
-                    <img class="card-img img-fluid" src="assets/images/small/img-6.jpg" alt="Card image">
-                    <div class="card-img-overlay">
-                        <h4 class="card-title text-white">Card title</h4>
-                        <p class="card-text text-light">This is a wider card with supporting text below as a
-                            natural lead-in to additional content. This content is a little bit
-                            longer.</p>
-                        <p class="card-text">
-                            <small class="text-white">Last updated 3 mins ago</small>
-                        </p>
-                    </div>
-                </div>
-            </div>
 
         </div>
 
