cdd85e23e08a544cd4ff0fa61c8ca535435fbf0b
Dodidee <nordiananorizan10@gmail.com>
2025-01-23 15:16:52 +0800

    update profile image

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -5,10 +5,14 @@
 
         <div class="row">
             <div class="col-lg-4">
-            <div class="card"><br>
-            <center>
-                    <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/dee.jpg') }}" alt="Card image cap">
-            </center>
+            <div class="card"></br>
+                <center>
+                    <img class="rounded-circle avatar-xl" 
+                         src="{{ (!empty($adminData->profile_image)) ? url('upload/admin_images/' . $adminData->profile_image) : url('upload/no_image.jpg') }}" 
+                         alt="Card image cap">
+                         
+                </center>
+                
                     <div class="card-body">
                         <h4 class="card-title">Name : {{ $adminData->name }}</h4>
                         <hr>
--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -41,15 +41,31 @@
                 </button>
             </div>
 
+            @php
+            $id = Auth::user()->id;
+            $adminData = App\Models\User::find($id);
+            @endphp
+
+
+
             <!-- User Dropdown -->
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-expanded="false">
                     <img class="rounded-circle header-profile-user"
-                        src="{{ asset('backend/assets/images/users/avatar-1.jpg') }}" alt="Header Avatar">
-                    <span class="d-none d-xl-inline-block ms-1">Julia</span>
+                        src="{{ (!empty($adminData->profile_image)) 
+                        ? url('upload/admin_images/' . $adminData->profile_image) : 
+                        url('upload/no_image.jpg') }}" alt="">
+                    <span class="d-none d-xl-inline-block ms-1">{{ $adminData->name}}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
+
+
+
+
+
+
+
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- Dropdown Items -->
                     <a class="dropdown-item" href="{{ route('admin.profile') }}"><i class="ri-user-line align-middle me-1"></i> Profile</a>
